b3066e8f848651ed36e55db19ad6323448809937
ludmilka-k <ludmilka.kurochkina@gmail.com>
2023-02-19 12:28:24 +0000

    Deploying to gh-pages from @ ludmilka-k/team-light-balance@b45bcf8804e636968649bd0dcb05f36c52563e83 🚀

---
+++ assets/room-price-modal-a031766a.js
@@ -0,0 +1,1 @@
+(()=>{const e={openModalpriceBtn:document.querySelector("[room-price-modal-open]"),closeModalpriceBtn:document.querySelector("[room-price-modal-close]"),roomPriceModal:document.querySelector("[room-price-modal]")};e.openModalpriceBtn.addEventListener("click",o),e.closeModalpriceBtn.addEventListener("click",o);function o(){e.roomPriceModal.classList.toggle("is-hidden")}})();
